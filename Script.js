27bffd43a87d807e2a5f78c190b375b36cb5f0b3
Doomblade712 <51216574+Doomblade712@users.noreply.github.com>
2022-10-01 15:56:55 -0500

    updated click funcitons

--- Script.js
+++ Script.js
@@ -1,9 +1,7 @@
-
-if (document.title.indexOf("Amazon") != -1) {
-    //console.log(document.getElementsByClassName("a-price-whole")[0].innerHTML);
-    //document.getElementsByClassName("a-price-whole")[0].innerHTML = "69";
+function OnClick(){
     const pricesClass = document.getElementsByClassName("a-price-whole");
     const pricesClassDecimal = document.getElementsByClassName("a-price-fraction");
+
     for (let i = 0; i < pricesClass.length; i++) {
         let whole = parseInt(pricesClass[i].innerHTML);
         let fraction = parseInt(pricesClassDecimal[i].innerHTML);
@@ -16,3 +14,20 @@ if (document.title.indexOf("Amazon") != -1) {
         pricesClassDecimal[i].innerHTML = pricesClassDecimal[i].innerHTML = `${fvalueFraction}`;
     }
 }
+
+function OffClick(){
+    const pricesClass = document.getElementsByClassName("a-price-whole");
+    const pricesClassDecimal = document.getElementsByClassName("a-price-fraction");
+    for (let i = 0; i < pricesClass.length; i++) {
+        let whole = parseInt(pricesClass[i].innerHTML);
+        let fraction = parseInt(pricesClassDecimal[i].innerHTML);
+        let total = whole+(fraction*0.01)
+        let tax = 0.065;
+        let fvalue = total - (total*tax);
+        let fvalueWhole = Math.round(fvalue);
+        let fvalueFraction = Math.round(((fvalue % 1) * 100));
+        pricesClass[i].innerHTML = pricesClass[i].innerHTML = `${fvalueWhole}`;
+        pricesClassDecimal[i].innerHTML = pricesClassDecimal[i].innerHTML = `${fvalueFraction}`;
+    }
+}
+
