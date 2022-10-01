71faa131ad40b80654be302edcaa49ecc3adcafe
Malik Danab <51216574+Doomblade712@users.noreply.github.com>
2022-10-01 14:29:30 -0500

    Test script

--- Script.js
+++ Script.js
@@ -1,9 +1,6 @@
 
 if (document.title.indexOf("Amazon") != -1) {
-    //Creating Elements
-    var btn = document.createElement("BUTTON")
-    var t = document.createTextNode("CLICK ME");
-    btn.appendChild(t);
-    //Appending to DOM 
-    document.body.appendChild(btn);
+    
+    document.getElementById("a-price-whole").class = "69";
+
 }
