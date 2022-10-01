2ad5d0106b91678d108a27215151052c0c6c1455
Malik Danab <51216574+Doomblade712@users.noreply.github.com>
2022-10-01 14:15:44 -0500

    added Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+
+if (document.title.indexOf("Amazon") != -1) {
+    //Creating Elements
+    var btn = document.createElement("BUTTON")
+    var t = document.createTextNode("CLICK ME");
+    btn.appendChild(t);
+    //Appending to DOM 
+    document.body.appendChild(btn);
+}
